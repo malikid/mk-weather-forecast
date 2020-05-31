5e75278e4de26c68e2eae7675d9f220718b75cdc
Glitch (hello-sqlite) <none>
2020-05-31 19:24:07 +0000

    🏕🏪 Checkpoint ./config-overrides.js:5526992/1495 ./package.json:5526992/3230 ./src/stores/weather-page.js:5526992/40001 ./src/config/index.js:5526992/102

---
+++ config-overrides.js
@@ -0,0 +1,37 @@
+const {
+  override,
+  addDecoratorsLegacy,
+  disableEsLint,
+  addBundleVisualizer,
+  addWebpackAlias,
+  adjustWorkbox,
+} = require("customize-cra");
+const path = require("path");
+
+module.exports = override(
+  // enable legacy decorators babel plugin
+  addDecoratorsLegacy(),
+
+  // disable eslint in webpack
+  disableEsLint(),
+
+  // add webpack bundle visualizer if BUNDLE_VISUALIZE flag is enabled
+  process.env.BUNDLE_VISUALIZE == 1 && addBundleVisualizer(),
+
+  // add an alias for "ag-grid-react" imports
+  addWebpackAlias({
+    Components: path.resolve(__dirname, "src/components"),
+    Pages: path.resolve(__dirname, "src/pages"),
+    Stores: path.resolve(__dirname, "src/stores"),
+    Styles: path.resolve(__dirname, "src/styles"),
+    Config: path.resolve(__dirname, "src/config")
+  }),
+
+  // adjust the underlying workbox
+  adjustWorkbox(wb =>
+    Object.assign(wb, {
+      skipWaiting: true,
+      exclude: (wb.exclude || []).concat("index.html")
+    })
+  )
+);
