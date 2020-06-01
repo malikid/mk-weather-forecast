0466c9b401c4e478cbfe2df731a5b31019de7f97
Glitch (hello-sqlite) <none>
2020-06-01 14:13:38 +0200

    Set up GA and HotJar.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,19 @@ import App from 'Components/App';
 import {Provider} from 'mobx-react';
 import store from 'Stores';
 
+import {GA_CONTAINER_ID, HOTJAR_ID, HOTJAR_VERSION} from 'Config';
+
+import ReactGA from 'react-ga';
+if (GA_CONTAINER_ID) {
+  ReactGA.initialize(GA_CONTAINER_ID);
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
+
+import {hotjar} from 'react-hotjar';
+if (HOTJAR_ID && HOTJAR_VERSION) {
+  hotjar.initialize(HOTJAR_ID, HOTJAR_VERSION);
+}
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
