f04c4e81ea615cc8212ad3c68f06b9a708f5f2ff
Glitch (hello-sqlite) <none>
2020-06-01 14:27:47 +0200

    Updated the API_KEY env var to REACT_APP_API_KEY in order to be passed into the project while building.

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
-export const API_KEY = process.env.API_KEY || '439d4b804bc8187953eb36d2a8c26a02';
+export const API_KEY = process.env.REACT_APP_API_KEY || '439d4b804bc8187953eb36d2a8c26a02';
 export const GA_CONTAINER_ID = process.env.REACT_APP_GA_CONTAINER_ID || '';
 export const HOTJAR_ID = process.env.REACT_APP_HOTJAR_ID || '';
 export const HOTJAR_VERSION = process.env.REACT_APP_HOTJAR_VERSION || '';
